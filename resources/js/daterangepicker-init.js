b979094a8e6706504ad3579922b0dfc615788a10
Fajar Maulana Firdaus <fajar.firdaus@xendit.co>
2024-06-03 13:43:50 +0700

    fix: moment import

--- resources/js/daterangepicker-init.js
+++ resources/js/daterangepicker-init.js
@@ -5,7 +5,10 @@ import 'daterangepicker';
 
 $(document).ready(function() {
     $('input[name="daterange"]').daterangepicker({
-        opens: 'left'
+        opens: 'left',
+        startDate: moment(),
+        endDate: moment().add(1, 'days'),
+        minDate: moment(),
     }, function(start, end, label) {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
